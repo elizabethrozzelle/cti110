6a8515658c13c5645b9f0ed9ff247ebcf5dc2d14
elizabethrozzelle <rozzelle4066@student.faytechcc.edu>
2025-06-16 17:48:55 -0400

    Add files via upload

---
+++ P1LAB1_RozzelleElizabeth.py
@@ -0,0 +1,10 @@
+# Elizabeth Rozzelle
+# June 13, 2025
+# P1LAB1 
+# This program prompts the user to enter their first and last name separately, then displays a personalized welcome message.
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
